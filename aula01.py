d6852aee97edc7ed8694d76defb390531dfe044a
Lorena Carmo <85353791+Oligarc@users.noreply.github.com>
2023-10-02 16:44:25 -0300

    first commit

---
+++ aula01.py
@@ -0,0 +1,13 @@
+nome = "Clara"
+sobrenome = "Matilda"
+idade = 14
+ano_nascimento = 2007
+maior_idade = 18
+altura_metros = 1.67
+
+print('Qual o seu nome?', nome)
+print('Seu sobrenome:', sobrenome)
+print('Qual a sua idade?', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('Atingiu maior idade?', idade >= maior_idade)
+print('Qual a sua altura?', altura_metros)
---
+++ aula02.py
@@ -0,0 +1,6 @@
+nome = 'Lorena'
+altura = 1.63
+peso = 80
+imc = peso // (altura * altura)
+
+print('Seu IMC:', imc)
